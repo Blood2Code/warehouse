8f25844cb82d9704719dd20daf14cb5ee9179fad
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-14 23:36:00 +0500

    [+] product repository created successfully

---
+++ src/main/java/com/example/wherehouse/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.wherehouse.repository;
+
+import com.example.wherehouse.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
