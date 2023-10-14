93e9433ba10d5e959a73201d0373f7417a28ceb6
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-14 23:34:22 +0500

    [+] organization repository creeted

---
+++ src/main/java/com/example/wherehouse/repository/OrganizationRepository.java
@@ -0,0 +1,9 @@
+package com.example.wherehouse.repository;
+
+import com.example.wherehouse.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
