7c8fcd86a2b48ca745a9d270e502fb0632eebe89
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-14 23:31:39 +0500

    [*] jwt filter corrected

--- src/main/java/com/example/wherehouse/security/JwtFilter.java
+++ src/main/java/com/example/wherehouse/security/JwtFilter.java
@@ -5,26 +5,26 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 @Component
-@RequiredArgsConstructor
-@Slf4j
-public class JwtFilter
-//        extends OncePerRequestFilter
-{
-/*
+public class JwtFilter extends OncePerRequestFilter {
+
     private final JwtUtil jwtService;
     private final UserDetailsService userDetailsService;
+
+    public JwtFilter(JwtUtil jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
@@ -48,5 +48,5 @@ public class JwtFilter
         }
         filterChain.doFilter(request, response);
     }
- */
+
 }
