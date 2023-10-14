991bcabb40638d49ace2b82fa26f4ffe243414e6
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-14 23:37:47 +0500

    [+] user repository created successfully

--- src/main/java/com/example/wherehouse/repository/UserRepository.java
+++ src/main/java/com/example/wherehouse/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.example.wherehouse.repository;
 
-import com.example.wherehouse.entity.UserLogin;
+import com.example.wherehouse.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
-public interface UserRepository extends JpaRepository<UserLogin, Long> {
-    Optional<UserLogin> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
