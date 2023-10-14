f24040558b33a151451339dacf3ac11256cfcae2
umar <abduxalilraxmonberdiyev@gmail.com>
2023-10-14 23:33:31 +0500

    [+] organization dto created successfully

--- src/main/java/com/example/wherehouse/entity/Organization.java
+++ src/main/java/com/example/wherehouse/entity/Organization.java
@@ -1,4 +1,24 @@
 package com.example.wherehouse.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity(name = "u_organization")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Organization {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    @ManyToOne
+    private User userLogin;
+    @OneToMany
+    private List<WareHouse> whereHouse;
+
 }
